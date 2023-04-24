7012097b415006f5a0494254f56f7e6c62134d67
AmirHPartovi <a.partovi99@gmail.com>
2023-04-24 17:05:24 +0330

    package.json config

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,4 +1,41 @@
-const planets = [];
+
+const fs = require('fs');
+const  path = require('path');
+const {parse} = require('csv-parse');
 
 
-module.exports = planets
+const habitablePlanet= [];
+const isHabitable =(planet)=>{
+    return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6 ;
+}
+const loadPlanetData = () =>{
+    return new Promise((resolve,reject)=>{
+
+        fs.createReadStream(path.join(__dirname , '..' , '..' , 'data' , 'kepler-data.csv'))
+        .pipe(parse({
+            comment :'#',
+            columns : true,
+        }))
+        .on('data' , (data)=>{
+            if(isHabitable(data)){
+                habitablePlanet.push(data);
+            } 
+        })
+        .on('error',(err)=>{
+            console.log(err);
+            reject(err)
+        })
+        .on('end',()=>{
+            // console.log(`${habitablePlanet.length} planets habitable found !`);
+            // console.log('habitable planet' , habitablePlanet);
+            resolve()
+        })
+    })
+}
+
+module.exports = {
+    planets : habitablePlanet,
+    loadPlanetData,
+}
