43fba94b1c5f861f9221dca5ee894a4f5fd4f8e7
AmirHPartovi <a.partovi99@gmail.com>
2023-04-24 19:54:06 +0330

    deploy on one orgin

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path')
 const cors = require('cors')
 const planetRouter = require('./routes/planets/planets.router');
 
@@ -8,8 +9,11 @@ app.use(cors({
     origin:'http://localhost:3000',
 }))
 app.use(express.json());
+app.use(express.static(path.join(__dirname,'..','static')))
 
-
+app.use('/',(res,req)=>{
+    res.sendFile(path.join(__dirname,'..','static','index.html'))
+})
 app.use('/planets', planetRouter)
 
 module.exports = app
