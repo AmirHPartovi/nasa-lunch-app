45946923c168542e6d2e82c06e3d1199470a2cd7
AmirHPartovi <a.partovi99@gmail.com>
2023-04-24 20:38:01 +0330

    lunches.model created

--- server/src/app.js
+++ server/src/app.js
@@ -1,19 +1,24 @@
 const express = require('express');
-const path = require('path')
-const cors = require('cors')
+const path = require('path');
+const cors = require('cors');
+const morgan = require('morgan');
+
 const planetRouter = require('./routes/planets/planets.router');
 
 const app = express();
 
 app.use(cors({
     origin:'http://localhost:3000',
-}))
+}));
+app.use(morgan('combined'));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'..','static')))
 
+
+app.use('/planets', planetRouter);
 app.use('/',(res,req)=>{
     res.sendFile(path.join(__dirname,'..','static','index.html'))
 })
-app.use('/planets', planetRouter)
 
 module.exports = app
---
+++ server/src/models/lunches.model.js
@@ -0,0 +1,16 @@
+const lunches = new Map();
+
+const lunch ={
+    flightNumber:100,
+    mission:'kepler Exploration X',
+    rocket:'Explorer IS1',
+    lunchDate: new Date('December 12 , 2035'),
+    planet: 'kepler-442 b',
+    customer:['NOVA','NASA'],
+    upcoming:true,
+    success:true,
+};
+
+lunches.set(lunch.flightNumber,lunch);
+
+module.exports = lunches
