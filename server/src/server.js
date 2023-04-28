d3c756b928a3693913654bfad163b987695bda64
AmirHPartovi <a.partovi99@gmail.com>
2023-04-28 09:11:31 +0330

    cluster & worker created

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,7 @@
 const http = require('http');
-const app = require('./app')
+const app = require('./app');
+const os = require('os');
+const cluster = require('cluster')
 
 const {loadPlanetData} = require('./models/planets.model')
 
@@ -7,12 +9,24 @@ const PORT = process.env.PORT || 8000;
 
 const server =http.createServer(app);
 
-async function startServer(){
-    await loadPlanetData();
+const numberOfCore = os.cpus().length;
+
+
+if(cluster.isMaster){
+    console.log('master has been started ...');
+    for(let i=0 ; i < numberOfCore ; i++){
+        cluster.fork()
+    }
+}else{
+    console.log('worker process started...');
+    async function startServer(){
+        await loadPlanetData();
+        
+        server.listen(PORT , ()=>{
+            console.log(`server listening on port ${PORT}...`);
+        });
+    }
     
-    server.listen(PORT , ()=>{
-        console.log(`server listening on port ${PORT}...`);
-    });
+    startServer()
 }
 
-startServer()
