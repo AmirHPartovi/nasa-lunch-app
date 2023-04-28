51cc3ab53bb93d55425cfb4d31b540bade944305
AmirHPartovi <a.partovi99@gmail.com>
2023-04-28 11:03:09 +0330

    pm2 package add & script write

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app');
-const os = require('os');
-const cluster = require('cluster')
+// const os = require('os');
+// const cluster = require('cluster')
 
 const {loadPlanetData} = require('./models/planets.model')
 
@@ -9,24 +9,33 @@ const PORT = process.env.PORT || 8000;
 
 const server =http.createServer(app);
 
-const numberOfCore = os.cpus().length;
+// const numberOfCore = os.cpus().length;
 
 
-if(cluster.isMaster){
-    console.log('master has been started ...');
-    for(let i=0 ; i < numberOfCore ; i++){
-        cluster.fork()
-    }
-}else{
-    console.log('worker process started...');
-    async function startServer(){
-        await loadPlanetData();
+// if(cluster.isMaster){
+//     console.log('master has been started ...');
+//     for(let i=0 ; i < numberOfCore ; i++){
+//         cluster.fork()
+//     }
+// }else{
+//     console.log('worker process started...');
+//     async function startServer(){
+//         await loadPlanetData();
         
-        server.listen(PORT , ()=>{
-            console.log(`server listening on port ${PORT}...`);
-        });
-    }
+//         server.listen(PORT , ()=>{
+//             console.log(`server listening on port ${PORT}...`);
+//         });
+//     }
     
-    startServer()
+//     startServer()
+// }
+
+async function startServer(){
+    await loadPlanetData();
+    
+    server.listen(PORT , ()=>{
+        console.log(`server listening on port ${PORT}...`);
+    });
 }
 
+startServer()
