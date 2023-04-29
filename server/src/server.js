55bf2919f2d1e7d304963035d9632b4eaba988b8
AmirHPartovi <a.partovi99@gmail.com>
2023-04-29 08:38:38 +0330

    mongoDB setup

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const app = require('./app');
+const mongoose = require('mongoose')
 // const os = require('os');
 // const cluster = require('cluster')
 
@@ -7,7 +8,35 @@ const {loadPlanetData} = require('./models/planets.model')
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = 'mongodb+srv://nasa-api:NTy526QofpViK0bK@nasa-cluster.ncrr8tz.mongodb.net/?retryWrites=true&w=majority'
+
 const server =http.createServer(app);
+
+mongoose.connection.once('open',()=>{
+    console.log('mongoDB connection is ready ...');
+})
+mongoose.connection.on('error',(err)=>{
+    console.error(err);
+})
+
+async function startServer(){
+    await mongoose.connect(MONGO_URL)
+    await loadPlanetData();
+    // await mongoose.connect(MONGO_URL,,{
+    //     useNewUrlParser : true ,
+    //     useFindAndModify : false ,
+    //     useCreateIndex : true ,
+    //     useUnifiedTypology:true,
+    // })
+    
+    server.listen(PORT , ()=>{
+        console.log(`server listening on port ${PORT}...`);
+    });
+}
+
+startServer()
+
+
 
 // const numberOfCore = os.cpus().length;
 
@@ -30,12 +59,5 @@ const server =http.createServer(app);
 //     startServer()
 // }
 
-async function startServer(){
-    await loadPlanetData();
-    
-    server.listen(PORT , ()=>{
-        console.log(`server listening on port ${PORT}...`);
-    });
-}
 
-startServer()
+
